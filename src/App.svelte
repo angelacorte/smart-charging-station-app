dab7d9d7b6c3b21aa90fe51eda3149ae1af569b0
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-25 17:14:14 +0200

    feat: add map to main app

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
-<script lang="ts">
+<script>
+  import Geolocation from "svelte-geolocation";
+  import Map from './lib/Map.svelte';
+
+  let getPosition = false;
+  let coords = [];
 </script>
 
 <main>
-  
-  <div class="card">
-  </div>
-
+  <button on:click={() => (getPosition = true)}> Get actual location </button>
+  <Geolocation
+          getPosition={getPosition}
+          bind:coords
+          let:loading
+          let:success
+          let:error
+          let:notSupported>
+    {#if notSupported}
+      Your browser does not support the Geolocation API.
+    {:else}
+      {#if loading}
+        Loading...
+      {:else if success}
+        <Map {coords} />
+      {:else if error}
+        An error occurred. {error.code} {error.message}
+      {/if}
+    {/if}
+  </Geolocation>
 </main>
