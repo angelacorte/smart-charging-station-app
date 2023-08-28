b9ea1b4a8abdd386108f3d043bfa637d53157d4d
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-28 12:44:01 +0200

    chore: update setup and plugins

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  prerender: {
+    enabled: false
+  },
+  ssr: false,
+};
+
+export default config;
