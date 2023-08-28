110dc8a22cc82b68e65d1ce86b824892728ae9b5
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-28 15:31:18 +0200

    feat: add dialog and function for start charging

--- src/routes/charging/[id]/+page.svelte
+++ src/routes/charging/[id]/+page.svelte
@@ -3,8 +3,18 @@
 
     let id = $page.params.id;
 
+    function startCharging() {
+        console.log(`send request for charging car at the station with id ` + id);
+    }
 </script>
 
 <h1>Charging page</h1>
 
 <h3>Charging station ID: {id}</h3>
+
+{#if id !== undefined}
+    <p>Do you want to start charging your car at this charging station?</p>
+    <button on:click={startCharging}>Yes</button>
+{:else}
+    <p>Charging station not found</p>
+{/if}
