c9ff64732c61a1b990300f0f0e7eb91f3f0a82b5
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-28 21:18:17 +0200

    feat: create personalized popup

---
+++ src/lib/components/MarkerPopup.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let station;
+</script>
+
+<div style="width:100%;text-align:center;font-weight:600">
+    <p>{station.provider} {station.name}</p>
+    <p>Status is {station.status}</p>
+    <br/>
+    {#if station.status === "FREE"}
+        <div>
+            <a class="btn p-" href="reserve/{station.id}">Reserve this charging station.</a>
+       </div>
+    {/if}
+</div>
