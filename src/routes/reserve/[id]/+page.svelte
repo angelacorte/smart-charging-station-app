2508a9a67a360cdce68797345f981c7a55bcf486
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-28 21:20:47 +0200

    feat: add reservation page

---
+++ src/routes/reserve/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { page } from '$app/stores';
+    // import {reserve} from "$lib/stores.ts";
+
+    let id = $page.params.id;
+
+    function askReserve() {
+        console.log(`send request for reserving the station with id ` + id);
+        // reserve(id)
+    }
+</script>
+
+<h1>Reserve page</h1>
+
+<h3>Reserving station ID: {id}</h3>
+
+{#if id !== undefined}
+    <p>Do you want to reserve this charging station?</p>
+    <button on:click={askReserve}>Yes</button>
+{:else}
+    <p>Charging station not found</p>
+{/if}
