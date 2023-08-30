6839d0cb4aef27ca21a30717fad624bb2df993db
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-30 23:51:30 +0200

    refactor: change index page into login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
+<script>
+    import Login from "$lib/Login.svelte";
+        import Register from '$lib/Register.svelte';
+
+        // Redirection to /profile
+        function redirectToHome() {
+        window.location.assign('/home')
+    }
+</script>
+
 <h1>Welcome to Smart Charging Stations!</h1>
 
-<div class="container mt-3 align-self-auto">
-    <a class="btn col-md-4 p-5" href="map">Click here to see stations nearby your position.</a>
-    <br/>
-    <a class="btn col-md-4 p-5" href="scanner">Click here to scan the QR code of a charging station.</a>
-</div>
+<Login on:success={redirectToHome} />
