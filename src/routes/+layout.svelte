7f80cbf9255aa3e05a24907f009147e3640231bd
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-31 18:34:45 +0200

    feat: update navbar and add logout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,67 @@
 <script>
     import '../app.css'
+    import { onMount } from "svelte";
+
+    // Show mobile icon and display menu
+    let showMobileMenu = false;
+
+    let isLogged = false
+    function logout() {
+        window.localStorage.clear();
+        isLogged = false
+        window.location.assign('/')
+    }
+
+    // Mobile menu click event handler
+     const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
+
+    // Media match query handler
+    const mediaQueryHandler = e => {
+        // Reset mobile state
+        if (!e.matches) {
+            showMobileMenu = false;
+        }
+    };
+
+    // Attach media query listener on mount hook
+    onMount(() => {
+        isLogged = window.localStorage.length > 0
+        const mediaListener = window.matchMedia("(max-width: 767px)");
+
+        mediaListener.addListener(mediaQueryHandler);
+    });
+
+    // List of navigation items
+    const navItems = [
+        { label: "Smart Charging Stations", href: "/home" },
+        { label: "Map", href: "/map" },
+        { label: "QR Scanning", href: "/scanner" },
+    ];
 </script>
 
-<nav class="navbar navbar-dark sticky-top d-flex justify-content-between">
-    <div class="container">
-        <a class="navbar-brand" href="/home">Smart Charging Stations</a>
-        <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>-->
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="map">Map</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="scanner">QR Code Scanner</a>
-                </li>
-            </ul>
+<nav>
+    <div class="inner">
+        <div on:click={handleMobileIconClick} class={`mobile-icon${showMobileMenu ? ' active' : ''}`}>
+            <div class="middle-line"></div>
         </div>
+        <ul class={`navbar-list${showMobileMenu ? ' mobile' : ''}`}>
+            {#if isLogged}
+                {#each navItems as item}
+                    <li>
+                        <a href={item.href}>{item.label}</a>
+                    </li>
+                {/each}
+                <li>
+                    <a on:click={logout}>Logout</a>
+                </li>
+            {:else}
+                <li>
+                    <a href="/">Smart Charging Stations</a>
+                </li>
+            {/if}
+
+        </ul>
     </div>
 </nav>
 
 <slot/>
-<!--
-<div id="app"></div>
-<script type="module" src="/src/main.ts"></script>-->
