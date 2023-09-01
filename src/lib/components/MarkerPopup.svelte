40561affa4734eaa6d8033168f42141ed8c64c71
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-09-01 15:46:39 +0200

    feat: add working system for adding or removing favourites

--- src/lib/components/MarkerPopup.svelte
+++ src/lib/components/MarkerPopup.svelte
@@ -1,12 +1,65 @@
 <script>
     import {onMount} from "svelte";
+    import FaRegStar from 'svelte-icons/fa/FaRegStar.svelte'
+    import FaStar from 'svelte-icons/fa/FaStar.svelte'
+    import {fetchStations, Stations, addFavourite, removeFavourite} from "../stores.ts";
+    import {load} from "../../routes/home/+page.svelte";
 
     export let station;
     let address = ""
 
-    onMount(() => {
-        translateCoordinatesToAddress();
+    export let favStations = []
+
+    export async function bindStations(){
+        await fetchStations()
+        favStations.forEach((elem, index) => {
+            favStations[index] = $Stations.find(station => {
+                return station.id === elem
+            });
+        })
+    }
+
+    export async function addToFavourites() {
+        let user = JSON.parse(window.localStorage.getItem("user"))
+        let res = await addFavourite({
+            userId: user._id,
+            chargingStationId: station.id
+        })
+        console.log("res ", res)
+
+        if (res){
+            console.log("yes")
+            user.chargingStations.push(station.id)
+            window.localStorage.setItem("user", JSON.stringify(user))
+            await updateFavourites()
+        }
+    }
+
+    export async function removeFromFavourites() {
+        let user = JSON.parse(window.localStorage.getItem("user"))
+        let res = await removeFavourite({
+            userId: user._id,
+            chargingStationId: station.id
+        })
+        console.log("res ", res)
+        if (res) {
+            console.log("yes")
+            user.chargingStations = user.chargingStations.filter(id => id !== station.id);
+            window.localStorage.setItem("user", JSON.stringify(user))
+            await updateFavourites()
+        }
+    }
+
+    onMount(async () => {
+        await translateCoordinatesToAddress();
+        await updateFavourites()
     });
+
+    async function updateFavourites(){
+        const { props } = await load({}); // Passa un oggetto vuoto poiché load non richiede parametri
+        favStations = props.stations;
+        await bindStations()
+    }
     async function translateCoordinatesToAddress() {
         try {
             const response = await fetch(
@@ -22,7 +75,12 @@
     }
 </script>
 
-<div style="width:100%;text-align:center;font-weight:600">
+<div style="width:100%;text-align:center;font-weight:600" >
+    {#if favStations.some(favStation => favStation.id === station.id)}
+        <div style="color: orange; width: 32px; height: 32px" on:click={removeFromFavourites}><FaStar></FaStar></div>
+    {:else }
+        <div style="color: orange; width: 32px; height: 32px" on:click={addToFavourites}><FaRegStar></FaRegStar></div>
+    {/if}
     <p>{station.provider} {station.name} ({station.voltage} kW)</p>
     <p>Status is {station.state}</p>
     <p>Address is "{address}"</p>
