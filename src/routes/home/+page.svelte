f1a8ffb854785630f6530bfe26d64936b3067e59
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-09-01 14:09:17 +0200

    feat: add table for favourite stations

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -8,26 +8,37 @@
         return {
             props: {
                 name: user.name,
-                email: user.email
+                stations: user.chargingStations
             }
         };
     }
+
 </script>
 
 <script>
     import { onMount } from 'svelte';
+    import {fetchStations, Stations} from "$lib/stores.ts";
 
-    let email, name;
+    let name;
+    let stations = []
 
     onMount(async () => {
+        await fetchStations()
         const { props } = await load({}); // Passa un oggetto vuoto poiché load non richiede parametri
-        email = props.email;
+        console.log("props ", props)
         name = props.name;
+        stations = props.stations;
+        stations.forEach((elem, index) => {
+            stations[index] = $Stations.find(station => {
+                return station.id === elem
+            });
+        })
+        console.log("stations ", stations)
     });
 </script>
 
 <h1>Profile</h1>
-<p>Hello {name} you are logged in with the email {email}</p>
+<h2>Welcome {name}!</h2>
 
 <div class="container">
     <a class="btn" href="map">See stations nearby your position.</a>
@@ -35,3 +46,21 @@
     <br/>
     <a class="btn" href="scanner">Scan the QR code of a charging station.</a>
 </div>
+
+
+<h3>Charging stations of interest</h3>
+{#if stations.length > 0}
+    <ul>
+        {#each stations as s}
+            <li>
+                <a style="color: #397367"  href="charge/{s.id}">{s.name} {s.provider}</a>
+                <br/>
+                <br/>
+            </li>
+        {/each}
+    </ul>
+
+{:else }
+    <p>There are no charging stations marked as favourite.</p>
+    <p>Add more!</p>
+{/if}
